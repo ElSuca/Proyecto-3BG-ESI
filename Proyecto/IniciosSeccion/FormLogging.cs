b38015f764682ecc258c90dfaabf62330f33f4e2
santiago garcia <santiagogarcia2728@gmail.com>
2022-06-03 00:12:02 -0300

    Avances en la api de autentificación

--- Proyecto/IniciosSeccion/FormLogging.cs
+++ Proyecto/IniciosSeccion/FormLogging.cs
@@ -1,27 +1,61 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using Proyecto.Model;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using Proyecto;
 
 namespace Proyecto.IniciosSeccion
 {
     public partial class FormLogging : Form
     {
+
         public FormLogging()
         {
             InitializeComponent();
+            
         }
-
+        string Username;
+        string Pasword;
+ 
         private void btnLoggin_Click(object sender, EventArgs e)
         {
+            MySqlConnection conexion = new MySqlConnection(
+                "server = 127.0.0.1; " +
+                "uid = root;" +
+                "pwd=root;" +
+                "database=proyecto"
+               );
 
+            conexion.Open();
+
+            var user = SetData();
+            SerializeJson(user);
         }
 
+        private static string _path = @"C:\Proyecto_3BG\Credenciales.json";
+        
+
+        public List<User> SetData()
+        {
+            Username = txtUserName.Text;
+            Pasword = txtPassword.Text;
+
+            new User
+            {
+                Name = Username,
+                Password = Pasword
+            };
+            return null;
+        } 
+        public void SerializeJson(List<User> users)
+        {
+            string userJson = JsonConvert.SerializeObject(users.ToArray(), Formatting.Indented);
+            File.WriteAllText(_path, userJson);
+        }
+        
        
     }
 }
