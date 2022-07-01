260ebdb858082846ce20720cacbf39307093659a
santiago garcia <santiagogarcia2728@gmail.com>
2022-07-01 11:29:29 -0300

    Adelanto de recivir datos para la clase

--- CapaLoogica/UserControler.cs
+++ CapaLoogica/UserControler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Net;
 using System.Text;
 using CapaDeDatos;
@@ -9,7 +10,7 @@ using Newtonsoft.Json;
 namespace CapaLogica
 {
     public class UserControler
-    {
+    { 
         public static void Alta(string nombre, string apellido, string telefono, string email, string password)
         {
             ModelUser p = new ModelUser();
@@ -55,37 +56,45 @@ namespace CapaLogica
         }
         public static bool Autenticar(string nombre, string password)
         {
-
+            string Url = "http://127.0.0.1:8888/autenticar/";
             ModelUser u = new ModelUser();
             UserControler uc = new UserControler();
-            u.Name = nombre;
-            uc.enviar(nombre, password);
+            uc.enviar(nombre, password,Url);
+            string respuesta = recivir(Url);
             return u.Autenticar(password);
         }
-        public void enviar(string nombre, string password)
+        public void enviar(string nombre, string password, string Url)
         {
             HttpListener listener = new HttpListener();
-            string Url = "http://127.0.0.1:8888/autenticar/";
             WebRequest request = WebRequest.Create(Url);
             listener.Prefixes.Add(Url);
             checklisten(listener);
-             HttpListenerContext context = listener.GetContext();
-            
+            HttpListenerContext context = listener.GetContext();
             HttpListenerResponse response = context.Response;
 
             string body;
             request.Method = "post";
             Dictionary<string, string> camposJsonDeSalida = new Dictionary<string, string>();
-             camposJsonDeSalida.Add(nombre, password);
+            camposJsonDeSalida.Add(nombre, password);
 
             body = JsonConvert.SerializeObject(camposJsonDeSalida);
 
             response.ContentType = "application/json";
             byte[] buffer = Encoding.UTF8.GetBytes(body);
             response.ContentLength64 = buffer.Length;
-            System.IO.Stream output = response.OutputStream;
+            Stream output = response.OutputStream;
             output.Write(buffer, 0, buffer.Length);
         }
+        public static string recivir(string Url)
+        {
+           
+            WebRequest request = WebRequest.Create(Url);
+            WebResponse response = request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            return reader.ReadToEnd().Trim();
+
+        }
+
         public void checklisten(HttpListener listener)
         {
             if (listener.IsListening) listener.Stop();
