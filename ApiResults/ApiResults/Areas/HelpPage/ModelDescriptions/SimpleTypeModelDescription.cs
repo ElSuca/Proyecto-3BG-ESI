e2bdbb65154e4427d56c8b14987ed98c9fbfb79a
PanaceaBathory <94203718+PanaceaBathory@users.noreply.github.com>
2022-11-06 19:01:41 -0300

    Delete ApiResults directory

--- ApiResults/ApiResults/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ApiResults.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- ApiResults/ApiResults/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ApiResults.WebApiApplication" Language="C#" %>
