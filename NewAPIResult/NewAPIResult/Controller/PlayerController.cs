1e8a7613442d3cd7095fcf8519cbd9d71ed4dd7e
PanaceaBathory <zikoar@protonmail.com>
2022-11-06 19:00:04 -0300

    New ApiResults

---
+++ NewAPIResult/NewAPIResult/Controller/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data;
+namespace NewAPIResult
+{
+    public class PlayerController : ApiController
+    {
+
+
+        
+        [HttpPost]
+
+        public List<ModelPlayer> GetAllPlayer([FromBody] JsonRequest p)
+        {
+            List<ModelPlayer> players = new List<ModelPlayer>();
+            for (int i = 1 + ((p.PageNumber - 1) * 5); i < p.PageNumber * 5; i++)
+            {
+                ModelPlayer player = new ModelPlayer();
+                player.PopulatePlayer(i);
+                players.Add(player);
+            }
+            return players;
+        }
+
+
+    }
+}
---
+++ NewAPIResult/NewAPIResult/Models/JsonRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewAPIResult
+{
+    public class JsonRequest
+    {
+        public int Id { get; set; }
+        public int PageNumber { get; set; }
+        public string SearchBarContent { get; set; }
+
+    }
+}
